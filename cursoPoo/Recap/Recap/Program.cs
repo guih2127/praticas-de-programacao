1ff1557b760e1d106d907e733af00b9706d261ac
guih2127 <guih2127@gmail.com>
2018-12-30 12:59:30 -0200

    remember strings in c#

---
+++ cursoPoo/Recap/Recap/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Recap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double saldo = 10.35784;
+            int idade = 32;
+            string nome = "Maria";
+
+            // SAÍDA DE DADOS
+            // formatação de decimais
+            Console.WriteLine(saldo.ToString("F1", CultureInfo.InvariantCulture));
+
+            // placeholders
+            Console.WriteLine("{0} tem {1} anos e saldo de {2:F} reais.", nome, idade, saldo);
+
+            // interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e saldo de {saldo:F2} reais.");
+
+            // ENTRADA DE DADOS
+            // split
+            string[] s = Console.ReadLine().Split(' ');
+            Console.WriteLine($"{s[0]}, {s[1]}, {s[2]}");
+
+        }
+    }
+}
